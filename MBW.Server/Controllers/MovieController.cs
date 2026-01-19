00eef3323668ba78490f9256c4d3e42e59f626c7
Maxwell <maxwell.wogsland@no.experis.com>
2026-01-19 10:27:14 +0100

    added slnx file back

--- MBW.Server/Controllers/MovieController.cs
+++ MBW.Server/Controllers/MovieController.cs
@@ -1,27 +1,26 @@
 ﻿using MBW.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MBW.Server.Controllers
+namespace MBW.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MovieController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MovieController : ControllerBase
-    {
-        // Dummy movies
-        List<Movie> movies = new List<Movie>
-        {
-            new Movie("The Matrix", 136) { Id = 1 },
-            new Movie("Inception", 148) { Id = 2 },
-            new Movie("Interstellar", 169) { Id = 3 },
-            new Movie("The Dark Knight", 152) { Id = 4 },
-            new Movie("Fight Club", 139) { Id = 5 }
-        };
+    // Dummy movies
+    List<Movie> movies = new List<Movie>
+    { 
+        new Movie("The Matrix", 136) { Id = 1 },
+        new Movie("Inception", 148) { Id = 2 }, 
+        new Movie("Interstellar", 169) { Id = 3 },
+        new Movie("The Dark Knight", 152) { Id = 4 },
+        new Movie("Fight Club", 139) { Id = 5 }
+    };
         
-        // GET: api/movie
-        [HttpGet]
-        public List<Movie> Get()
-        {
-            return movies;
-        }
-    }
+    // GET: api/movie
+    [HttpGet]
+    public async Task<List<Movie>> Get()
+    {
+        return movies;
+    } 
 }
---
+++ MBW.Server/Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+﻿using MBW.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MBW.Server.Controllers;
+
+[Route("api/[controller]")]
+
+public class PostsController : Controller
+{
+    // GET: api/posts
+    [HttpGet]
+    public List<Post> GetAllPosts()
+    {
+        return null;
+    }
+    
+    // GET: api/post?{userId} i think is it
+    [HttpGet("{userId}")]
+    public List<Post> GetUserPosts(int userId)
+    {
+        return null;
+    }
+    
+    // POST:
+    
+    
+    // PUT:
+    
+    // DELETE:
+}
