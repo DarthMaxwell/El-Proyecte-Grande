82f6f236420a816311a37240ecad861e778d6b5e
Maxwell <maxwell.wogsland@no.experis.com>
2026-01-22 13:39:22 +0100

    Finished Movie controller and added exception andling for the database to be offline

--- MBW.Server/Controllers/MovieController.cs
+++ MBW.Server/Controllers/MovieController.cs
@@ -20,8 +20,32 @@ public class MovieController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<List<Movie>>> Get()
     {
-        List<Movie> res = await _dbContext.Movies.ToListAsync();
-        
-        return Ok(res);
-    } 
+        try
+        {
+            List<Movie> res = await _dbContext.Movies.ToListAsync();
+            return Ok(res);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable, "Database is currently unavailable. Please try again later.");
+        }
+    }
+    
+    // GET: api/movie/topfive
+    [HttpGet("topfive")]
+    public async Task<ActionResult<List<Movie>>> GetTopFive()
+    {
+        try
+        {
+            List<Movie> res = await _dbContext.Posts.GroupBy(p => p.MovieId).OrderByDescending(g => g.Count())
+                .Take(5).Select(g => g.Key).Join(_dbContext.Movies, id => id, m => m.Id, (id, m) => m)
+                .ToListAsync();
+
+            return Ok(res);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable, "Database is currently unavailable. Please try again later.");
+        }
+    }
 }
