8e4cfe084920323256ec6506d4e3f791fe2bfc60
Maxwell <maxwell.wogsland@no.experis.com>
2026-01-28 11:41:19 +0100

    Comments setting up api test methods

---
+++ ServerTests/AuthControllerTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerTests;
+
+public class AuthControllerTests
+{
+    //Login_NoUserWithUsername_ReturnUnauthorized
+    //Login_UserNotValidPassword_ReturnUnauthorized
+    //Login_UserValidPassword_ReturnOkAndLoginResponse
+    
+    //Register_UsernameUsed_ReturnBadRequest
+    //Register_ValidUsernameAndPassword_ReturnCreatedAndRegistrationResponse
+}
