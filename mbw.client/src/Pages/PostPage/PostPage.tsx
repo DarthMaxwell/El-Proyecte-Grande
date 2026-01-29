524ecae7cb3bdcc8fdc69fadcfd362c5ee24a577
Maxwell <maxwell.wogsland@no.experis.com>
2026-01-29 11:14:22 +0100

    removed old imp of postpage

--- mbw.client/src/Pages/PostPage/PostPage.tsx
+++ mbw.client/src/Pages/PostPage/PostPage.tsx
@@ -1,17 +1,7 @@
-import Post from "../../Components/Post/Post";
-
-type Post = {
-    id: string;
-    title: string;
-    body: string;
-    movie: { id: number; title: string };
-    author: { id: string; username: string };
-};
-
 export default function PostPage() {
     
 
     return (
-        <Post Id={1} MovieTitle={"Absolute Cinema"} Username={"testing"} Content={"This is not from the database just a test"}/>
+        "Implementation: PostPage"
     );
 }
